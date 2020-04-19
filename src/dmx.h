d2205a463926e769eb56b537a407ef396f6f9a30
sasodoma <saso.doma@gmail.com>
2020-04-19 16:03:54 +0200

    Include two libraries and move includes from source.
    This commit adds two libraries that are required to compile the source file, and also moves libraries from the source file to the header file because their data types are used here.

--- src/dmx.h
+++ src/dmx.h
@@ -1,3 +1,11 @@
+#include <stdint.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
+
 #ifndef DMX_h
 #define DMX_h
 
