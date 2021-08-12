16a56743bc6dc78f42af37aabc2058ac6cbfee5e
luksal <luksal@users.noreply.github.com>
2021-08-12 14:00:48 +0200

    Added GNUGPLv3 license headers

--- src/dmx.cpp
+++ src/dmx.cpp
@@ -1,4 +1,20 @@
 #include <dmx.h>
+/* 
+ * This file is part of the ESP32-DMX distribution (https://github.com/luksal/ESP32-DMX).
+ * Copyright (c) 2021 Lukas Salomon.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #define DMX_SERIAL_INPUT_PIN    16          // pin for dmx rx
 #define DMX_SERIAL_OUTPUT_PIN   17          // pin for dmx tx
